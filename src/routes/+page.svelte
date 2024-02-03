f42be477e9a2d33e6035e10bf5001a087a8779be
lagisreal89 <56201840+lagisreal89@users.noreply.github.com>
2024-02-03 16:22:52 -0500

    home page somewhat
    added img and changed text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+    import image from "$lib/assets/national museum.webp"
     import { goto } from "$app/navigation";
 </script>
-
-<h1>Home Page</h1>
-<p>Information about the product would be listed here</p>
+<style>
+    p {text-align: center;}
+    p {font-size: 20px;}
+</style>
+<h1 style="font-family:'Papyrus'">A Walk Through History</h1>
+<p>Locations of places are at the button called Go to App</p>
+<img src={image} alt="National Museum of African American History and Culture" width = "50%" height = "50%">
+<p>&nbsp</p>
 <button on:click={() => goto("./map")}>Go to App</button>
+
