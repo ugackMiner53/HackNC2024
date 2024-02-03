99c4eb1d8decdc09ee351fcad82eb017f7706faf
ugackMiner <ugackMiner@gmail.com>
2024-02-03 12:41:46 -0500

    Created starter svelte project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    // import { writable } from "svelte/store";
+    
+    let thing = false;
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Thing is {thing}</p>
+<button on:click={() => {thing = !thing;}}>click me</button>
