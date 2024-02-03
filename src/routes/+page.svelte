aa53f3205d009903ce3d384e5436b2073ec29852
lagisreal89 <56201840+lagisreal89@users.noreply.github.com>
2024-02-03 16:29:07 -0500

    homepage edit
    changed text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,11 @@
 </script>
 <style>
     p {text-align: center;}
-    p {font-size: 20px;}
+    p1 {font-size: 20px;}
+    p {font-size: 15px;}
 </style>
 <h1 style="font-family:'Papyrus'">A Walk Through History</h1>
+<p1>Easiest website to find your favorite African American History Museum</p1>
 <p>Locations of places are at the button called Go to App</p>
 <img src={image} alt="National Museum of African American History and Culture" width = "50%" height = "50%">
 <p>&nbsp</p>
