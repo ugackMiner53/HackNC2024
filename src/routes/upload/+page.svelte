723784aca95c9caee57c92718faa0261926e1208
Ziyang Qiu <danktha0s@gmail.com>
2024-02-03 14:32:09 -0500

    uploading files

---
+++ src/routes/upload/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { enhance } from '$app/forms';
+  import type { ActionData } from './$types';
+
+  const goodExt = ['.jpg', '.jpeg', '.png', '.webp'];
+
+  export let form: ActionData;
+</script>
+
+<svelte:head>
+  <title>SvelteKit file upload</title>
+</svelte:head>
+
+<main>
+  <h1>upload</h1>
+
+  <form method="post" use:enhance enctype="multipart/form-data">
+    <label for="file">Upload your file</label>
+    <input type="file" id="file" name="fileToUpload" accept={goodExt.join(',')} required />
+
+    <button type="submit">Submit</button>
+  </form>
+
+  {#if form?.error}
+    boo
+  {/if}
+
+  {#if form?.success}
+    yay
+  {/if}
+</main>
