fbedfdf9ca9871a6bf3c0dfb411116425bd8eeda
lagisreal89 <56201840+lagisreal89@users.noreply.github.com>
2024-02-04 02:41:11 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,13 +14,12 @@
         font-size: 23px;    
         margin: 0 3px;
         cursor: pointer;
-     }
-    p {text-align: center;}
-    p1 {font-size: 20px;}
-    p {font-size: 15px;}
+    }
+    h1 {font-size: 25px;}
+    h2 {font-size: 20px;}
 </style>
 <h1 style="font-family:'Papyrus'">A Walk Through History</h1>
-<p1>Easiest website to find your favorite African American History Museum</p1>
+<h2>You don't have to be in a museum to walk through history</h2>
 <p>&nbsp</p>
 <button class="buttons" type="button" on:click={() => goto("./contacts")}>Contact Us</button>
 <button class="buttons" type="button" on:click={() => goto("./about")}>About Us</button>
