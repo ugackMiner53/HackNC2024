1797975d02180686ac06427d3530ea783faf3fc5
lagisreal89 <56201840+lagisreal89@users.noreply.github.com>
2024-02-03 22:07:44 -0500

    contact us page
    created form with samson including textbox for message, name, email
    resets after submit is clicked

---
+++ src/routes/contacts/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import ContactUs from "$lib/components/ContactUs.svelte";
+    import image from "$lib/assets/bg.webp";
+</script>
+  
+<main>
+    <body>
+        <h1>Contact Us</h1>
+        <ContactUs />
+    </body>
+</main>
+<style>
+    body{background-image: url("https://jobs.globalpayments.com/media/sowbjwkj/gpn_brand-mastheads_netspend-nextep-xenial.jpg?anchor=center&mode=crop&width=1600&height=900&mode=crop&quality=75");
+background-repeat: no-repeat; background-attachment: fixed;background-size: 100% 100%;}
+    h1{text-align: center;}
+    h1{font-family: 'Times New Roman', Times, serif;}
+    h1{color: whitesmoke;}
+</style>
